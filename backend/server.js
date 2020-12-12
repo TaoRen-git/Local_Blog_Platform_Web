0e48472ec9d29719b6244eb84558c0e95ad1ed1e
SCP-D14134 <rentx@dukes.jmu.edu>
2020-12-12 09:46:12 -0500

    move server.sj to backend folder

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const Article = require('./models/article')
-const articleRouter = require('./routes/articles')
+const Article = require('../models/article')
+const articleRouter = require('../routes/articles')
 const methodOverride = require('method-override');
 const app = express()
 
 
-mongoose.connect('mongodb://localhost/blogss', 
+mongoose.connect('mongodb://localhost/Blog', 
                 { useNewUrlParser: true, 
                   useUnifiedTopology: true, 
                   useCreateIndex:true })
